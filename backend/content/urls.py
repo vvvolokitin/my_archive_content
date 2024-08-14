99588f77744ffe1a0518be8b3154431520190d7d
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-08-14 16:39:33 +0500

    Add pages

--- backend/content/urls.py
+++ backend/content/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from content.views import Home
+from content.views import Home, movie, game, serial, book
 
 app_name = 'content'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('movie/', movie, name='movie'),
+    path('serial/', serial, name='serial'),
+    path('game/', game, name='game'),
+    path('book/', book, name='book'),
 ]
--- backend/content/views.py
+++ backend/content/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
 from django.views import generic
 
+from content.models import Movie, Serial, Game, Book
+
 
 class Home(generic.TemplateView):
     """Домашняя страница."""
     template_name = 'content/home.html'
+
+
+def movie(request):
+    """Страница фильмов."""
+
+    template = 'content/content.html'
+    object_list = Movie.objects.all()
+    context = {
+        'object_list': object_list
+    }
+    return render(request, template, context)
+
+
+def serial(request):
+    """Страница сериалов."""
+
+    template = 'content/content.html'
+    object_list = Serial.objects.all()
+    context = {
+        'object_list': object_list
+    }
+    return render(request, template, context)
+
+
+def game(request):
+    """Страница игр."""
+
+    template = 'content/content.html'
+    object_list = Game.objects.all()
+    context = {
+        'object_list': object_list
+    }
+    return render(request, template, context)
+
+
+def book(request):
+    """Страница книг."""
+
+    template = 'content/content.html'
+    object_list = Book.objects.all()
+    context = {
+        'object_list': object_list
+    }
+    return render(request, template, context)
